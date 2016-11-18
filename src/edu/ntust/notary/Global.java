d3a9a20fdeb2c2742eaa93ffa26bd32cdae58d5a
amao1991 <M10409310@mail.ntust.edu.tw>
2016-11-18 23:36:36 +0800

    add notary

---
+++ src/edu/ntust/notary/Global.java
@@ -0,0 +1,19 @@
+package edu.ntust.notary;
+
+public class Global {	
+	public final static String SERVER_URL = "********";
+	public final static String DB_NAME = "********";
+	public final static String USER = "********";
+	public final static String PASS = "********";
+	public final static String SECURE_VALUE = "********";
+	
+	// time = 0, 時間後, 
+	// time = 1, 時間前, 
+	// time = 2, 時間區間
+	// time = 3, 不辨識
+	public final static String time_restrict_id = "0";
+	
+	//	0, 不辨識
+	//	1, 要辨識
+	public final static String count_or_not = "1";
+}
